7fef18a8174c36ea36e0316e44f459715eb0bb68
Janine <janinegrey1994@gmail.com>
2023-05-20 21:27:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+  const addtocart = (number) => {
+    // Retrieve the quantity from the input field
+    const InputQ = document.getElementById(InputQ +  productName);
+    const quantity = parseInt(InputQ.value)};
+  
+   
