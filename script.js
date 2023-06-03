4afa6fe6d63f10a05c620bfb0f20955584620517
Janine <janinegrey1994@gmail.com>
2023-06-03 17:44:36 +0200

    JavaScript: Added cart management functionality and added local storage support. Handle updating cart items, Save cart value to local storage, Set input values from local storage.
    HTML files:
    index.html, products.html & cart.html :
    CSS Styling,
    Product images added to cart,
    Cart Remove Item button added,
    Price,
    Grand Total,
    Footer.

--- script.js
+++ script.js
@@ -1,7 +1,67 @@
 
-  const addtocart = (number) => {
-    // Retrieve the quantity from the input field
-    const InputQ = document.getElementById(InputQ +  productName);
-    const quantity = parseInt(InputQ.value)};
-  
-   
+// const cartItems = []; // ['snake', 'spider']
+let cartItems = { // object to store cart items
+   snake: {
+      quantity: 0
+   },
+   spider: {
+      quantity: 0
+   },
+    peachLily: {
+        quantity: 0
+    },
+    aloeVera: {
+        quantity: 0
+    },
+    rubberPlant: {
+        quantity: 0
+    },
+}
+
+loadCartFromLS()  //loads saved quantity cart
+
+updateInputItems()  // updates and saves changes in quantities
+
+function addToCart (productName) {
+    console.log(productName, document.getElementById(productName), +'') // 'snake' | 'spider' | etc.. //
+
+    if (+document.getElementById(productName).value >= 0) {  //ensures that you can't select negative values.  // use a plus to cast a string to a number
+
+        if (productName === 'snake') {
+            const inputValue = document.getElementById('snake').value || 0  //cart should not be less than 0
+            console.log(inputValue)
+            cartItems.snake.quantity = parseInt(inputValue)
+        } else if (productName === 'spider') {
+
+        } else if (productName === 'peachLily') {
+
+        } else if (productName === 'aloeVera') {
+
+        } else if (productName === 'rubberPlant') {
+
+        }
+    }
+    //update and safe after action
+    updateInputItems()
+    saveCartToLS()
+    console.log(cartItems, localStorage.getItem('cartItems'))
+}
+
+function loadCartFromLS() {
+    const localStorageCartItems = localStorage.getItem('cartItems');
+    if(localStorageCartItems) {
+        cartItems = JSON.parse(localStorageCartItems)  //parse is opposite of parseInt
+    }
+}
+
+function saveCartToLS() {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems))
+}
+
+function updateInputItems() {
+    document.getElementById('snake').value = cartItems.snake.quantity
+    document.getElementById('spider').value = cartItems.spider.quantity
+    document.getElementById('peachLily').value = cartItems.peachLily.quantity
+    document.getElementById('aloeVera').value = cartItems.aloeVera.quantity
+    document.getElementById('rubberPlant').value = cartItems.rubberPlant.quantity
+}
